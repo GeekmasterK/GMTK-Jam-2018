bb8893bdbee12367dd63e4a0ccf1837d75be61ff
DESKTOP-EQ1UVHE\Kevin <ktheiss1990@gmail.com>
2018-08-31 18:06:41 -0700

    Added GameManager and SceneTransition objects and scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager manager;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+        }
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+    //public Animator sceneAnim;
+    public string sceneName;
+
+    Scene currentScene;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        if(currentScene.name == "_InitScene")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /*IEnumerator LoadScene()
+    {
+        sceneAnim.SetTrigger("end");
+        yield return new WaitForSeconds(0.15f);
+        SceneManager.LoadScene(sceneName);
+    }*/
+}
